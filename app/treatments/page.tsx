52d63638babef87ea3792de36521601a63068d05
crowddevelopers08-arch <crowddevelopers08@gmail.com>
2026-05-26 12:42:46 +0530

    smile baby treatments lp added code

--- app/treatments/page.tsx
+++ app/treatments/page.tsx
@@ -1,3 +1,6 @@
+import React from 'react'
+import Script from 'next/script'
+
 import FertilityHero from '@/component/treatment/FertilityHero'
 import FertilityBanner from '@/component/treatment/herosection'
 import Navbar from '@/component/treatment/nabat'
@@ -7,13 +10,28 @@ import CostBanner from '@/component/treatment/sectiontwo'
 import ComparisonBanner from '@/component/treatment/tablecomponent'
 import TestimonialsSection from '@/component/treatment/TestimonialsSection'
 import FAQSection from '@/component/treatment/FAQSection'
-import React from 'react'
 import Footer from '@/component/treatment/footer'
 import MobileActionBar from '@/component/treatment/mobile-bar'
 
 const page = () => {
   return (
     <div>
+      {/* Google Ads Tag */}
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=AW-17991432817"
+        strategy="afterInteractive"
+      />
+
+      <Script id="google-ads-tag" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'AW-17991432817');
+        `}
+      </Script>
+
       <MobileActionBar />
       <Navbar />
       <FertilityBanner />
--- app/treatments/thank-you/page.tsx
+++ app/treatments/thank-you/page.tsx
@@ -1,15 +1,41 @@
+import React from 'react'
+import Script from 'next/script'
+
 import Headers from '@/component/smilebaby/smile-navbar'
 import Footer from '@/component/treatment/footer'
 import ThankYouPage from '@/component/treatment/thankpage'
-import React from 'react'
 
 const page = () => {
   return (
     <div>
-       <Headers />
-       <ThankYouPage />
-        <Footer />
-       
+      {/* Google Ads Conversion Tracking */}
+      <Script id="google-conversion" strategy="afterInteractive">
+        {`
+          function gtag_report_conversion(url) {
+            var callback = function () {
+              if (typeof(url) != 'undefined') {
+                window.location = url;
+              }
+            };
+
+            gtag('event', 'conversion', {
+              'send_to': 'AW-17991432817/noj0CJejuZ8cEPH0_YJD',
+              'value': 1.0,
+              'currency': 'INR',
+              'event_callback': callback
+            });
+
+            return false;
+          }
+
+          // Auto trigger on thank you page load
+          gtag_report_conversion();
+        `}
+      </Script>
+
+      <Headers />
+      <ThankYouPage />
+      <Footer />
     </div>
   )
 }
